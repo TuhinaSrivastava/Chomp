b583bce336d00f80b6d4cb191d7fc58a01fd49cc
tuhinasrivastava <tuhina_srivastava24@milton.edu>
2022-11-14 15:08:40 -0500

    Completed Reducer and Incorporated Reducer in List of Boards

---
+++ src/Board.java
@@ -0,0 +1,21 @@
+/*public class Board {
+    public int xpos;
+    public int ypos;
+
+    public boolean isAlive;
+    public static int bla;
+
+    public int sqr(int x){
+        return (x*x);
+    }
+
+    public Board(int a, int b){
+        xpos=a;
+        ypos=b;
+        isAlive=true;
+    }
+    public void print() {
+        System.out.println("xpos=" + xpos + "ypos=" + ypos) {
+
+        }
+    }*/
--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -8,68 +8,116 @@ public class MyPlayer {
         columns = new int[10];
         int min = 0;
         int max = 3;
-        reducer();
+        reducer(3, 3, 3);
 
-        int random_int = (int) Math.floor(Math.random() * (max - min + 1) + min);
+        /*int random_int = (int) Math.floor(Math.random() * (max - min + 1) + min);
         int random_int2 = (int) Math.floor(Math.random() * (max - min + 1) + min);
         int random_int3 = (int) Math.floor(Math.random() * (max - min + 1) + min);
-        System.out.println(random_int + "-" + random_int2 + "-" + random_int3);
+        System.out.println(random_int + "-" + random_int2 + "-" + random_int3);*/
 
 
         for (int i = 1; i < 4; i++) {
-            for (int x = 0; x < 4; x++) {
-                for (int y = 0; y < 4; y++) {
-                    if (i >= x && i >= y && x >= y) {
-                        System.out.println(i + "-" + x + "-" + y);
-                    }
-                    if (i == 1 && x == 0 && y == 0) {
-                        System.out.println("losing board!");
+            for (int h = 0; h < 4; h++) {
+                for (int g = 0; g < 4; g++) {
+                    if (i >= h && i >= g && h >= g) {
+                        System.out.println("REDUCER");
+                        System.out.println(i + "-" + h + "-" + g);
+                        reducer(i, h, g);
                     }
                 }
             }
         }
     }
-    private static void reducer() {
-        for (int a = 3; a > 1; a--) {
-            
-        }
-            for(int b = 3; b > 1; b--){
+
+    public void reducer(int x, int y, int z) {
+        if (z > 0) {
+
+            for (int c = z - 1; c > -1; c--) {
+
+                System.out.println(x + "-" + y + "-" + c);
 
             }
-                for(int c = 3; c > 0; c--){
+            for (int b = y - 1; b > -1; b--) {
+                if (b > z) {
+                    System.out.println(x + "-" + b + "-" + z);
+                } else {
+                    System.out.println(x + "-" + b + "-" + b);
 
                 }
-                    //if(a>=b && a>=c && b>=c){
-                        //System.out.println(a + "-" + b + "-" + c);
-                    //}
+            }
+            for (int a = x - 1; a > 0; a--) {
+                if (a > y && y > z) {
+                    System.out.println(a + "-" + y + "-" + z);
+                } else {
+                    if (z > a) {
+                        System.out.println(a + "-" + a + "-" + a);
 
-    }
+                    } else {
+                        System.out.println(a + "-" + a + "-" + z);
+                    }
+                }
 
-    //add your code to return the row and the column of the chip you want to take.
-    //you'll be returning a data type called Point which consists of two integers.
+            }
+        } else if (y > 0 && z == 0) {
+            for (int b = y - 1; b > -1; b--) {
+                System.out.println(x + "-" + b + "-" + z);
+            }
 
-    public Point move(Chip[][] pBoard) {
-
-
-        gameBoard = pBoard;
-        int column = 0;
-        int row = 0;
-
-        row = 1;
-        column = 1;
-
-        Point myMove = new Point(row, column);
-
-
-
-
-        return myMove;
-    }
-
-
+            for (int a = x - 1; a > 0; a--) {
+                if (a > y) {
+                    System.out.println(a + "-" + y + "-" + z);
+                } else {
+                    System.out.println(a + "-" + a + "-" + z);
+                }
+            }
 
 
-
+        }else{
+            for(int a = x-1; a> 0; a--){
+                System.out.println(a + "-" + y + "-" + z);
+            }
         }
+        if (x == 1 && y == 0 && z == 0) {
+            System.out.println("Losing Board!");
+        }
+        if (x == 1 && y == 1 && z == 0){
+            System.out.println("Winning Board!");
+        }
+    }
+
+
+
+            //if(a>=b && a>=c && b>=c){
+            //System.out.println(a + "-" + b + "-" + c);
+            //}
+
+
+
+        //add your code to return the row and the column of the chip you want to take.
+        //you'll be returning a data type called Point which consists of two integers.
+
+        public Point move (Chip[][]pBoard){
+
+
+            gameBoard = pBoard;
+            int column = 0;
+            int row = 0;
+
+            row = 1;
+            column = 1;
+
+            Point myMove = new Point(row, column);
+
+
+            return myMove;
+        }
+
+}
+
+
+
+
+
+
 
 
